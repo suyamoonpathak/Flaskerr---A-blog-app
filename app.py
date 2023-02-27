d67a0fcbead7047bdfa27b4cb94ca8f6f2b5244c
Suyamoon Pathak <partha.pathak23@gmail.com>
2023-02-28 00:12:40 +0530

    siteFiles:app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from siteFiles import my_app
+from siteFiles import app
 
-app = my_app()
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
--- siteFiles/__init__.py
+++ siteFiles/__init__.py
@@ -8,35 +8,32 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
-def my_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = "secret"
-    # UPLOAD_FOLDER = '/Users/suyamoonpathak/Desktop/IITM BlogLiteApp/siteFiles/static/images/uploads'
-    UPLOAD_FOLDER = path.join(path.dirname(
-        path.realpath(__file__)), 'static/images/uploads')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+# UPLOAD_FOLDER = '/Users/suyamoonpathak/Desktop/IITM BlogLiteApp/siteFiles/static/images/uploads'
+UPLOAD_FOLDER = path.join(path.dirname(
+    path.realpath(__file__)), 'static/images/uploads')
 
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
-    db.init_app(app)
+db.init_app(app)
 
-    from .views import views
-    from .authentication import authentication
+from .views import views
+from .authentication import authentication
 
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(authentication, url_prefix="/")
+app.register_blueprint(views, url_prefix="/")
+app.register_blueprint(authentication, url_prefix="/")
 
-    from .models import User, Post, Comment, Like
+from .models import User, Post, Comment, Like
 
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
-    login_manager = LoginManager()
-    login_manager.login_view = "authentication.login"
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = "authentication.login"
+login_manager.init_app(app)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
-    return app
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
